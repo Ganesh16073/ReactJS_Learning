d1b553d8b4f81587e08e04c1d3f41b4f4c3d4675
Ganesh_Kulkarni <ganeshkulkarni4237@gmail.com>
2024-12-16 19:49:41 +0530

    Created First Class Component

---
+++ 01_firstClassComponentProject/src/Component/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import React, { Component } from 'react';
+
+class Nav extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>First Class Component</h1>
+            </div>
+        );
+    }
+}
+
+export default Nav;
