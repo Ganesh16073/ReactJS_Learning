da78a3e67c5f02d45c9f803da0b547ecd211d4ea
Ganesh_Kulkarni <ganeshkulkarni4237@gmail.com>
2024-12-19 11:14:43 +0530

    implementing state count

--- 01_firstClassComponentProject/src/Component/Home.jsx
+++ 01_firstClassComponentProject/src/Component/Home.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 
 export default class Home extends React.Component
 {
+    constructor()
+    {
+        super();
+        this.state={
+            count:0
+        }
+    }
+    incValByOne =()=>
+    {
+        this.setState({count:this.state.count+1});
+    }
     render()
     {
-        return(
-            <h1>Home Page</h1>
+        console.log("component render");
+
+	return(
+        <>
+            <h1>Count value is {this.state.count}  </h1>
+            <input type='button' name='s' value='Increment Value By 1' onClick={this.incValByOne}/>
+        </>
         );
+
     }
 }
