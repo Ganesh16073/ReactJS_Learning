bd3885df89869cf99cf4761dca73e44d31b304d7
Ganesh_Kulkarni <ganeshkulkarni4237@gmail.com>
2025-01-04 10:47:50 +0530

    implemented bg_changer project with using states

--- 03_PropesChai/src/App.jsx
+++ 03_PropesChai/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
       <h1 className='bg-green-700 text-black p-4 rounded-xl mb-4'>Tailwint Test</h1>
       <Card channel="Giris Tech Hub" someObj={myObj}/>
-      <Card channel="Ganesh"/>   {/* if we dont pass props object then if we try to acces then it give error */}
+      {/* <Card channel="Ganesh"/>   if we dont pass props object then if we try to acces then it give error */}
     </>
   )
 }
